d4ce76f6b10a355c5f1b74f5d864a1fb649e34c0
Arthur Wisz <schwart@o2.pl>
2021-07-20 22:06:32 +0200

    Initial project setup

---
+++ make_requirements.py
@@ -0,0 +1,4 @@
+import os
+
+os.system("python -m piptools compile")
+os.system("python -m piptools compile dev-requirements.in")
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+# Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
+setup(
+    name="restuml2code",
+    install_requires=[
+        "lark-parser"
+    ],
+    extras_require={
+    },
+)
